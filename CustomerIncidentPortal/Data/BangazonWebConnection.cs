b123d7adb3db5a29777b666cf049643252c23f8b
James Regnier <James Regnier>
2016-11-10 13:42:20 -0600

    Created the data connection file in the data directory

---
+++ CustomerIncidentPortal/Data/BangazonWebConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace BangazonIncident.Connections
+{
+    public class BangazonWebConnexction
+    {
+        private string _connectionString = Environment.GetEnvironmentVariable("NTABangazonWebPath");
+
+        public void insert(string query)
+        {
+            SqliteConnection dbcon = new SqliteConnection(_connectionString);
+
+            dbcon.Open();
+            SqliteCommand dbcmd = dbcon.CreateCommand();
+
+            dbcmd.CommandText = query;
+            dbcmd.ExecuteNonQuery();
+
+            // clean up
+            dbcmd.Dispose();
+            dbcon.Close();
+        }
+
+        public void execute(string query, Action<SqliteDataReader> handler)
+        {
+
+            SqliteConnection dbcon = new SqliteConnection(_connectionString);
+
+            dbcon.Open();
+            SqliteCommand dbcmd = dbcon.CreateCommand();
+            dbcmd.CommandText = query;
+
+            using (var reader = dbcmd.ExecuteReader())
+            {
+                handler(reader);
+            }
+
+            // clean up
+            dbcmd.Dispose();
+            dbcon.Close();
+        }
+    }
+}
